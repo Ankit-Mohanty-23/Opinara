498c006ad39920b13131a78363fe37351404df22
Ankit Mohanty <ankitmsb760@gmail.com>
2025-12-09 02:34:59 +0530

    Add vote controller with post and comment voting functionality

--- backend/controller/vote.controller.js
+++ backend/controller/vote.controller.js
@@ -51,7 +51,62 @@ export async function postVote(req, res) {
     console.log("Error adding vote: ", error);
     return res.status(500).json({
       success: false,
-      message: "Internal server error while adding vote",
+      message: "Internal server error while voting post",
     });
   }
 }
+
+/**
+ * @desc    Handle Vote for a post
+ * @route   PATCH /:commentId/vote
+ * @access  Private
+ */
+
+export async function commentVote(req, res) {
+  try {
+    const userId = req.user?._id;
+    const { commentId } = req.params;
+    const { action } = req.body;
+
+    const existingVote = await Vote.findOne({
+      userId,
+      targetId: commentId,
+      targetType: "Comment",
+    }).exec();
+
+    let updatedVote;
+    if(existingVote){
+      if(existingVote.type === action){
+        await existingVote.deleteOne();
+        updatedVote = null;
+      }else{
+        updatedVote = await Vote.findByIdAndUpdate(
+          existingVote._id,
+          { type: action },
+          { new: true }
+        );
+      }
+    } else {
+      updatedVote = await Vote.create({
+        userId,
+        targetId: commentId,
+        targetType: "comment",
+        type: action,
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "Comment vote updated.",
+      data: updatedVote,
+    });
+
+  } catch (error) {
+    console.error("Error voting on comment: ", error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error while voting comment",
+    });
+  }
+}
+
