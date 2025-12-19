64f9eb0fcf84b73cfbbb1c0b2cb5e986156d6d78
Ankit Mohanty <ankitmsb760@gmail.com>
2025-12-20 04:54:14 +0530

    feat: add wave validation schemas for wave creation, posts retrieval, and location updates

--- backend/src/validation/wave.validation.js
+++ backend/src/validation/wave.validation.js
@@ -7,7 +7,7 @@ const objectIdSchema = z.string().refine(
     }
 );
 
-export const createWaveSchema ={
+export const createWaveSchema = {
     body: z.object({
         name: z.string().trim().min(1, "Wave name is required"),
         description: z.string().trim(),
@@ -15,5 +15,40 @@ export const createWaveSchema ={
     user: z.object({
         _id: objectIdSchema,
     }),
-    
-}
+};
+
+export const getWavePostSchema = {
+    user: z.object({
+        _id: objectIdSchema,
+    }),
+
+    params: z.object({
+        waveId: objectIdSchema,
+    }),
+};
+
+export const getLocationSchema = {
+    params: z.object({
+        waveId: objectIdSchema,
+    }),
+
+    user: z.object({
+        _id: objectIdSchema,
+    }),
+
+    body: z.object({
+        latitude: z.number({
+            required_error: "Latitude is required",
+            invalid_type_error: "Latitude must be a number",
+        })
+        .min(-180, "Longitude must be >= -180")
+        .max(180, "Longitude must be <= 180"),
+
+        latitude: z.number({
+            required_error: "Latitude is required",
+            invalid_type_error: "Latitude must be a number",
+        })
+        .min(-90, "Latitude must be >= -90")
+        .max(90, "Latitude must be <= 90"),
+    }),
+};
