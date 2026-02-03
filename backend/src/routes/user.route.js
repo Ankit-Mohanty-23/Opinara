a12eb264492762ba1c59b06396cbbad08d867219
Ankit Mohanty <ankitmsb760@gmail.com>
2026-02-03 22:09:50 +0530

    feat(user): add user routes with validation and auth middleware

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -23,18 +23,18 @@ import { validate, validateFile } from "../middleware/validate.middleware.js";
 const Router = express.Router();
 
 // Public
-Router.post("/signup", validate(loginSchema), signup);
-Router.post("/login", validate(signupSchema), login);
+Router.post("/signup", validate(signupSchema), signup);
+Router.post("/login", validate(loginSchema), login);
 
 // Protected
 Router.post("/verify", validate(getOtpSchema), sendOtp);
 Router.get("/data", auth, validate(getUserIdSchema), getUser);
 Router.delete("/delete", auth, validate(getUserIdSchema), deleteUser);
 Router.put("/bio", auth, validate(getBioSchema), addBio);
-Router.post(
+Router.patch(
   "/profile-pic",
   auth,
-  upload.single("profile_pic"),
+  upload.single("profilePic"),
   validateFile(profilepicSchema),
   addProfilePic
 );
